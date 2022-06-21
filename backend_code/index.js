99502f4c48e876963458c7593c0a9b900d07273d
JelenaVitosevic <vitosevicjelena83997@gmail.com>
2022-06-21 13:32:48 +0200

    Forgot to commit last days, some bugs have been fixed

--- backend_code/index.js
+++ backend_code/index.js
@@ -1,6 +1,9 @@
 const Joi = require('joi');
 const { joiPassword } = require('joi-password');
 
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 const express = require('express');
 const app = express();
 
@@ -8,8 +11,10 @@ app.use(express.json());
 
 const users = [];
 
+//let errorMessage = ''
+
 app.get('/', (req, res) => {
-    res.send('Hello Dejo!')
+    res.send('Jecin API')
 })
 
 app.get('/register', (req, res) => {
@@ -21,31 +26,35 @@ app.post('/register', (req, res) => {
         const newEmail = await req.body.email 
         for (let i=0; i<users.length; i++) {
             if (newEmail === users[i].email) {
-                throw new Error('Email allready exist!')
+                errorMessage = "email is not valid"
             }
         }
     }*/
 
     const schema = Joi.object({
-                        firstName: Joi.string().min(3).required(),
-                        lastName: Joi.string().required(),
-                        email: Joi.string().min(6).required().email(),//.external(checkEmail),
-                        password: joiPassword
-                                    .string()
-                                    .minOfSpecialCharacters(1)
-                                    .minOfLowercase(1)
-                                    .minOfUppercase(1)
-                                    .minOfNumeric(1)
-                                    .noWhiteSpaces()
-                                    .required(),
-                    })
+        firstName: Joi.string().min(3).required(),
+        lastName: Joi.string().required(),
+        email: Joi.string().min(6).required().email(),//.external(checkEmail),
+        password: joiPassword
+                    .string()
+                    .minOfSpecialCharacters(1)
+                    .minOfLowercase(1)
+                    .minOfUppercase(1)
+                    .minOfNumeric(1)
+                    .noWhiteSpaces()
+                    .required(),
+    })
 
-    const result = schema.validate(req.body)//validateAsync(req.body);
-    console.log(result)
-    if (result.error) {
+const result = schema.validate(req.body)//validateAsync(req.body);
+console.log(result)
+
+if (result.error) {
         res.status(404).send(result.error.details[0].message)
         return;
     }
+/*else if (errorMessage) {
+       res.status(404).send(errorMessage)
+    }*/ 
 
     const user = {
         id: users.length + 1,
@@ -54,21 +63,49 @@ app.post('/register', (req, res) => {
         email: req.body.email,
         password: req.body.password
     }
+    
+    
     users.push(user);
-    res.send(user)
+        res.send(user);  
 })
 
-/*app.get('/api/users/:id', (req, res) => {
-    const user = users.find(c => c.id === parseInt(req.params.id))
-    if (!user) res.status(404).send('The user with the given ID was not found!')
-    res.send(user)
-})*/
+app.post('/login', (req, res) => {
+    console.log(req)
+    const schema = Joi.object({
+        email: Joi.required(),
+        password: joiPassword.required(),
+    })
 
-app.get('/api/users/:email', (req, res) => {
-    const user = users.find(c => c.email === req.params.email)
-    if (!user) res.status(404).send('The user with the given email was not found!')
-    res.send(user)
+    const result = schema.validate(req.body);
+    if (result.error) {
+    res.status(404).send(result.error.details[0].message)
+    return;
+    }
+
+    const loginUser = {
+        email: req.body.email,
+        password: req.body.password
+    }
+    console.log(loginUser)
+    
+    for (i=0; i<users.length; i++) {
+        console.log(users[i].email)
+        console.log(req.body.email)
+
+        if (req.body.email === users[i].email) {
+            if (req.body.password === users[i].password) {
+                res.status(200).send('Welcome ' + users[i].firstName + ' ' + users[i].lastName + '!' +' You are loged in!')
+                console.log(users[i].email + 'true')
+
+            }
+            else {
+                res.status(401).send('You entered incorect password! Try again!')
+            }
+        }
+        
+        
+    }
 })
 
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 5000
 app.listen(port, () => console.log(`Listening on port ${port}...`))
